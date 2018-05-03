cadab18678e321e5f26d33fee20108fb72281833
Daniel Park <dpeace926@gmail.com>
2018-05-03 10:49:18 -0400

    build buytickets comp w/imgs

--- components/BuyTickets.js
+++ components/BuyTickets.js
@@ -2,25 +2,46 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 function BuyTickets() {
   return (
-    <View style={styles.test}>
+    <View style={styles.buyTickets}>
       <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>Hamburger</Text>
-        <Text style={styles.headerText}>Logo</Text>
+        <Image source={require('../styles/imgs/mta_logo.png')} />
+        {/* <Text style={styles.headerText}>Hamburger</Text> */}
       </View>
       <View style={styles.buyTicketContainer}>
         <View>
-          <View>
-
-          </View>
-          <View>
+          <View style={styles.button}>
+            <Image style={styles.buttonImg} source={require('../styles/imgs/buy-ticket.png')} />
             <Text style={styles.buttonText}>
               Buy Tickets
             </Text>
+            <Image style={styles.buttonArrow} source={require('../styles/imgs/right-arrow.png')} />
+          </View>
+          <View style={styles.button}>
+            <Image style={styles.buttonImg} source={require('../styles/imgs/wallet.png')} />
+            <Text style={styles.buttonText}>
+              Ticket Wallet
+            </Text>
+            <Image style={styles.buttonArrow} source={require('../styles/imgs/right-arrow.png')} />
+          </View>
+          <View style={[styles.button, styles.blueButton]}>
+            <Image style={styles.buttonImg} source={require('../styles/imgs/top_ticket.png')} />
+            <Text style={styles.blueButtonText}>
+              Your top tickets will appear here for quick access
+            </Text>
+            <View style={styles.emptyRight}></View>
+          </View>
+          <View style={styles.button}>
+            <Image style={styles.buttonImg} source={require('../styles/imgs/train_time.png')} />
+            <Text style={styles.buttonText}>
+              Train Time
+            </Text>
+            <Image style={styles.buttonArrow} source={require('../styles/imgs/right-arrow.png')} />
           </View>
         </View>
       </View>
@@ -29,32 +50,63 @@ function BuyTickets() {
 }
 
 const styles = StyleSheet.create({
-  test: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#F5FCFF',
+  buyTickets: {
+
   },
   headerContainer: {
-    // position: enum('absolute'),
-    // width: '100%',
     flexDirection: 'row',
-    backgroundColor: '#223365',
-    borderColor: 'yellow',
-    borderWidth: 2
+    // backgroundColor: '#223365',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    borderBottomColor: 'black',
+    borderBottomWidth: 2
+    // borderColor: 'yellow',
+    // borderWidth: 2
   },
   headerText: {
     color: 'white',
-    marginTop: 20,
   },
   buyTicketContainer: {
     backgroundColor: '#223365',
     alignSelf: 'stretch',
-    borderColor: 'orange',
-    borderWidth: 2
+    marginTop: 10,
+    // borderColor: 'orange',
+    // borderWidth: 2
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 5,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  buttonImg: {
+    width: 50,
+    flexGrow: 1,
   },
   buttonText: {
-    color: 'white'
+    color: 'black',
+    fontSize: 20,
+    flexGrow: 50,
+  },
+  buttonArrow: {
+    flexGrow: 1,
+  },
+  blueButton: {
+    backgroundColor: '#445588',
+    marginTop: -2
+  },
+  blueButtonText: {
+    color: 'white',
+    fontSize: 16,
+    maxWidth: 280
+  },
+  emptyRight: {
+    width: 50,
+    // borderColor: 'yellow',
+    // borderWidth: 2
   }
 });
 
